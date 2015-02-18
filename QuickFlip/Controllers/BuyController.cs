4934aa71ced511569c4cd1ee05b256826be52f49
jamesmaila <jamesmaila9@gmail.com>
2015-02-18 16:50:23 -0600

    added buy controller end point

---
+++ QuickFlip/Controllers/BuyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QuickFlip.BusinessLayer;
+using QuickFlip.Models;
+
+namespace QuickFlip.Controllers
+{
+    public class BuyController : Controller
+    {
+        public ActionResult Index(CommunityAbbrev id)
+        {
+            Community comm = BusinessLogic.GetCommunityByCommunityId((int)id);
+            return View(comm);
+        }
+
+    }
+}
