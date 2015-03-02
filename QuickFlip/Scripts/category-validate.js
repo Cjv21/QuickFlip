288d4bd9fbd9bb60e551ccaf8882940a0ddfc507
jamesmaila <jamesmaila9@gmail.com>
2015-03-02 00:15:13 -0600

    cleaned up a bunch of front end html css and javascript, tweaked some things

---
+++ QuickFlip/Scripts/category-validate.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+
+    var checkboxes = $("input[type='checkbox']");
+    var submitButt = $("input[type='submit']");
+
+    checkboxes.click(function () {
+        var error = $('label[for="checkbox-validate"]');
+
+        if (checkboxes.filter(':checked').length > 0) {
+            submitButt.prop("disabled", false);
+            error.hide();
+        } else {
+            submitButt.prop("disabled", true);
+            error.show();
+        }
+    });
+
+});
+
+
