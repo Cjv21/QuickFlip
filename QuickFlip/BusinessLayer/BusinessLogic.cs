1f3452d14c8c38346fceb019b8a06912e6ba8f78
jamesmaila <jamesmaila9@gmail.com>
2015-04-10 17:54:55 -0500

    almost done with recommendations, finshed matching my for sales with want to buys. still have to match my want to buys to other for sales, and my for sales with similar for sales....also fixed simple membership in home index

--- QuickFlip/BusinessLayer/BusinessLogic.cs
+++ QuickFlip/BusinessLayer/BusinessLogic.cs
@@ -347,6 +347,93 @@ namespace QuickFlip.BusinessLayer
 
         #endregion
 
+        #region Recommendations
+        public static List<Post> GetRecommendationsByUserId(int userId)
+        {
+            List<Post> recommendations = new List<Post>();
+
+            // posts i have created
+            List<Post> myPosts = GetPostsByUserId(userId);
+            List<Post> myForSale = myPosts.Where(x => x.PostType == PostType.Sell).ToList();
+            List<Post> myWantToBuy = myPosts.Where(x => x.PostType == PostType.Buy).ToList();
+
+            // posts i have bid on
+            List<Post> postsIBidOn = GetPostsByBidderId(userId);
+            List<Post> postsIWantToBuy = postsIBidOn.Where(x => x.PostType == PostType.Sell).ToList();
+            List<Post> postsIWantToSell = postsIBidOn.Where(x => x.PostType == PostType.Buy).ToList();
+
+            // GET POSTS THAT ARE LOOKING TO BUY WHAT I AM SELLING
+            List<Post> wantToBuyMyStuff = new List<Post>();
+            if (myForSale.Count != 0)
+            {
+                wantToBuyMyStuff = GetWantToBuyMyStuff(myForSale);
+
+                recommendations.AddRange(wantToBuyMyStuff);
+            }
+
+            return recommendations;
+        }
+
+        public static List<Post> GetWantToBuyMyStuff(List<Post> myForSale)
+        {
+            User me = GetUserByUserId(myForSale.First().UserId);
+
+            List<Post> buyPosts = GetAllPosts().Where(x => x.PostType == PostType.Buy).ToList();
+
+            // remove posts that aren't in my community and are not willing to ship
+            buyPosts.RemoveAll(x => 
+                x.UserId == me.UserId || 
+                (x.CommunityId != me.CommunityId && 
+                 x.TransactionType == TransactionType.Local)
+            );
+
+            // get posts in want to buy that match my for sale
+            List<Post> wantToBuyMyStuff = new List<Post>();
+            foreach (var post in myForSale)
+            {
+                wantToBuyMyStuff.AddRange(GetSimilarPosts(post, PostType.Buy));
+            }
+
+            return wantToBuyMyStuff;
+        }
+        
+        public static List<Post> GetSimilarPosts(Post postToBeSimilarTo, PostType type)
+        {
+            List<Post> posts = GetAllPosts();
+            posts.RemoveAll(x => (x.CommunityId != postToBeSimilarTo.CommunityId && x.TransactionType == TransactionType.Local));
+            posts.RemoveAll(x => x.PostType != type);
+
+            List<Post> similarPosts = new List<Post>();
+            List<string> keywords = new List<string>();
+            // no tags, generate search words
+            if (postToBeSimilarTo.Tags.Count == 0)
+            {
+                keywords = GetKeywords(postToBeSimilarTo);
+            }
+            // use tags as search words
+            else
+            {
+                keywords = postToBeSimilarTo.Tags;
+            }
+
+            foreach (var post in posts)
+            {
+                foreach (var word in keywords)
+                {
+                    if (post.Title.ToLower().Contains(word.ToLower()) ||
+                        post.Description.ToLower().Contains(word.ToLower()) ||
+                        post.Tags.Contains(word.ToLower()))
+                    {
+                        similarPosts.Add(post);
+                    }
+                }
+            }
+
+            return similarPosts.GroupBy(x => x.PostId).Select(g => g.First()).ToList();
+        }
+
+        #endregion
+
         #region Utilities
 
         public static string Generate8CharNonce()
@@ -412,6 +499,30 @@ namespace QuickFlip.BusinessLayer
             * Alltel – cellnumber@message.alltel.com
             */
         }
+
+     
+        public static List<string> GetKeywords(Post post)
+        {
+            string text = post.Title + " " + post.Description;
+
+            string[] stop = { "a", "able", "about", "above", "abroad", "according", "accordingly", "across", "actually", "adj", "after", "afterwards", "again", "against", "ago", "ahead", "ain't", "all", "allow", "allows", "almost", "alone", "along", "alongside", "already", "also", "although", "always", "am", "amid", "amidst", "among", "amongst", "an", "and", "another", "any", "anybody", "anyhow", "anyone", "anything", "anyway", "anyways", "anywhere", "apart", "appear", "appreciate", "appropriate", "are", "aren't", "around", "as", "a's", "aside", "ask", "asking", "associated", "at", "available", "away", "awfully", "b", "back", "backward", "backwards", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "begin", "behind", "being", "believe", "below", "beside", "besides", "best", "better", "between", "beyond", "both", "brief", "but", "by", "c", "came", "can", "cannot", "cant", "can't", "caption", "cause", "causes", "certain", "certainly", "changes", "clearly", "c'mon", "co", "co.", "com", "come", "comes", "concerning", "consequently", "consider", "considering", "contain", "containing", "contains", "corresponding", "could", "couldn't", "course", "c's", "currently", "d", "dare", "daren't", "definitely", "described", "despite", "did", "didn't", "different", "directly", "do", "does", "doesn't", "doing", "done", "don't", "down", "downwards", "during", "e", "each", "edu", "eg", "eight", "eighty", "either", "else", "elsewhere", "end", "ending", "enough", "entirely", "especially", "et", "etc", "even", "ever", "evermore", "every", "everybody", "everyone", "everything", "everywhere", "ex", "exactly", "example", "except", "f", "fairly", "far", "farther", "few", "fewer", "fifth", "first", "five", "followed", "following", "follows", "for", "forever", "former", "formerly", "forth", "forward", "found", "four", "from", "further", "furthermore", "g", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got", "gotten", "greetings", "h", "had", "hadn't", "half", "happens", "hardly", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "hello", "help", "hence", "her", "here", "hereafter", "hereby", "herein", "here's", "hereupon", "hers", "herself", "he's", "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit", "however", "hundred", "i", "i'd", "ie", "if", "ignored", "i'll", "i'm", "immediate", "in", "inasmuch", "inc", "inc.", "indeed", "indicate", "indicated", "indicates", "inner", "inside", "insofar", "instead", "into", "inward", "is", "isn't", "it", "it'd", "it'll", "its", "it's", "itself", "i've", "j", "just", "k", "keep", "keeps", "kept", "know", "known", "knows", "l", "last", "lately", "later", "latter", "latterly", "least", "less", "lest", "let", "let's", "like", "liked", "likely", "likewise", "little", "look", "looking", "looks", "low", "lower", "ltd", "m", "made", "mainly", "make", "makes", "many", "may", "maybe", "mayn't", "me", "mean", "meantime", "meanwhile", "merely", "might", "mightn't", "mine", "minus", "miss", "more", "moreover", "most", "mostly", "mr", "mrs", "much", "must", "mustn't", "my", "myself", "n", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needn't", "needs", "neither", "never", "neverf", "neverless", "nevertheless", "new", "next", "nine", "ninety", "no", "nobody", "non", "none", "nonetheless", "noone", "no-one", "nor", "normally", "not", "nothing", "notwithstanding", "novel", "now", "nowhere", "o", "obviously", "of", "off", "often", "oh", "ok", "okay", "old", "on", "once", "one", "ones", "one's", "only", "onto", "opposite", "or", "other", "others", "otherwise", "ought", "oughtn't", "our", "ours", "ourselves", "out", "outside", "over", "overall", "own", "p", "particular", "particularly", "past", "per", "perhaps", "placed", "please", "plus", "possible", "presumably", "probably", "provided", "provides", "q", "que", "quite", "qv", "r", "rather", "rd", "re", "really", "reasonably", "recent", "recently", "regarding", "regardless", "regards", "relatively", "respectively", "right", "round", "s", "said", "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming", "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "since", "six", "so", "some", "somebody", "someday", "somehow", "someone", "something", "sometime", "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub", "such", "sup", "sure", "t", "take", "taken", "taking", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that", "that'll", "thats", "that's", "that've", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "there'd", "therefore", "therein", "there'll", "there're", "theres", "there's", "thereupon", "there've", "these", "they", "they'd", "they'll", "they're", "they've", "thing", "things", "think", "third", "thirty", "this", "thorough", "thoroughly", "those", "though", "three", "through", "throughout", "thru", "thus", "till", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly", "try", "trying", "t's", "twice", "two", "u", "un", "under", "underneath", "undoing", "unfortunately", "unless", "unlike", "unlikely", "until", "unto", "up", "upon", "upwards", "us", "use", "used", "useful", "uses", "using", "usually", "v", "value", "various", "versus", "very", "via", "viz", "vs", "w", "want", "wants", "was", "wasn't", "way", "we", "we'd", "welcome", "well", "we'll", "went", "were", "we're", "weren't", "we've", "what", "whatever", "what'll", "what's", "what've", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "where's", "whereupon", "wherever", "whether", "which", "whichever", "while", "whilst", "whither", "who", "who'd", "whoever", "whole", "who'll", "whom", "whomever", "who's", "whose", "why", "will", "willing", "wish", "with", "within", "without", "wonder", "won't", "would", "wouldn't", "x", "y", "yes", "yet", "you", "you'd", "you'll", "your", "you're", "yours", "yourself", "yourselves", "you've", "z", "zero", "" };
+
+            char[] splitChars = { ' ', '\'' };
+
+            string[] words = text.Split(splitChars);
+
+            var filteredWords = words.Except(stop).ToList();
+
+            var keywordCount = (
+                from keyword in filteredWords
+                group keyword by keyword into g
+                select new { Keyword = g.Key, Count = g.Count() }
+             );
+
+            return keywordCount.OrderByDescending(k => k.Count).ThenByDescending(k => k.ToString().Length)
+                .Select(k => k.Keyword).Take(5).ToList();
+        }
+
 
         #endregion
     }
--- QuickFlip/Controllers/HomeController.cs
+++ QuickFlip/Controllers/HomeController.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Security;
-using DotNetOpenAuth.AspNet;
-using Microsoft.Web.WebPages.OAuth;
 using WebMatrix.WebData;
 using QuickFlip.BusinessLayer;
 using QuickFlip.Models;
+using System.Transactions;
+using System.Web.Security;
+using DotNetOpenAuth.AspNet;
+using Microsoft.Web.WebPages.OAuth;
+using QuickFlip.Filters;
 
 namespace QuickFlip.Controllers
 {
     public class HomeController : Controller
     {
+        [InitializeSimpleMembership]
         public ActionResult Index()
         {
             return View();
