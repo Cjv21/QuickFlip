8d19921e37168249602c5dd3f9332fa6124953df
jamesmaila <jamesmaila9@gmail.com>
2015-04-13 21:41:36 -0500

    category-select-all restored

---
+++ QuickFlip/Scripts/category-select-all.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+
+    $('#category-select-all').click(function (event) {
+        if (this.checked) {
+            $('#categories').find(':checkbox').each(function () {
+                this.checked = true;
+            });
+        }
+        else {
+            $('#categories').find(':checkbox').each(function () {
+                this.checked = false;
+            });
+        }
+    });
+
+});
+
+
